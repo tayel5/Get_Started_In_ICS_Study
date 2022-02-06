c30daec28469dd4554d21848efc7691fb5d3ebba
Eldon <taylor.eldon@gmail.com>
2022-02-05 23:09:42 -0700

    LPTHW exercise 36 dev

---
+++ Learn_Python3_THW/exercise_36.py
@@ -0,0 +1,60 @@
+def cafeteria():
+    print("You are at the Cafeteria, you win!!!")
+
+def admin():
+    print("You are at the administration building")
+
+def gym():
+    print("You are at the gym")
+
+def vent():
+    print("Stuck in the vent with the monkey, you should've chang'd your response")
+
+def courtyard(num):
+    print("You are at the courtyard")
+
+def library(num):
+    print("You are now at the library")
+
+def sleep(why):
+    print(why)
+    exit(0)
+
+def confusion(why,num):
+    print(why)
+    start(num)
+
+def start(attempt):
+    print("What do you you do?")
+
+    choice = input("(left)(right)> ")
+
+    if choice == "left":
+        library(attempt)
+    elif choice == "right":
+        courtyard(attempt)
+    elif choice == "vent":
+        vent()
+    elif choice == "sleep":
+        sleep("You go back to sleep and deal with it another day")
+    elif attempt == 0:
+        attempt += 1
+        confusion("Not sure what you're doing there...", attempt)
+    elif attempt == 1:
+        attempt += 1
+        confusion("Did you hit your head or something?", attempt)
+    elif attempt == 2:
+        attempt += 1
+        confusion("No, not like that.", attempt)
+    elif attempt == 3:
+        attempt += 1
+        confusion("Please refrain from creating multiple timelines without adequate felt goatees", attempt)
+    elif attempt == 4:
+        attempt += 1
+        confusion("You're wrinkling my brain.", attempt)
+    else:
+        confusion("Clear your mind.", attempt)
+
+
+num = 0
+start(num)
---
+++ Learn_Python3_THW/exercise_36_TODO.md
@@ -0,0 +1,28 @@
+Rooms to code:
+Study Room
+Secret Vent exit
+Library
+    - Key location
+        - search bookshelves
+        - open spanish 101 book
+        - inside is a key, take the key
+Courtyard
+    - central location to get to rest of campus
+Gymnasium
+    - build required strength to open cafeteria door
+        - take PEE class
+            - Teachers teaching to teach, couches coaching to coach montage
+Administration building
+    - defeat Dean boss
+        - perform karaoke
+        - buy an oculus rift
+Cafeteria
+    - If Dean has not been defeated he will intercept you on the way
+    - Win condition
+        - required to open door to cafeteria:
+            Strength (gymnasium)
+            key (library)
+            Defeat Dean
+
+
+![img_1.png](img_1.png)
